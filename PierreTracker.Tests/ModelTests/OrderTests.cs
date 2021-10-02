6c148e07f886159f8f48e7c8ef980a6a78bb65db
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-10-02 19:35:08 -0400

    fail test to get title

--- PierreTracker.Tests/ModelTests/OrderTests.cs
+++ PierreTracker.Tests/ModelTests/OrderTests.cs
@@ -11,9 +11,23 @@ namespace PierreTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order testOrder = new Order();
+      Order testOrder = new Order("Argument1", "Argument2", 3.0M);
       Assert.AreEqual(typeof(Order), testOrder.GetType());
     }
+    [TestMethod]
+      public void GetTitle_ReturnsTitle_String()
+    {
+      //Arrange
+      string title = "Cider Donuts";
+      string description = "Donuts made of cider and sugar";
+      decimal price = 3.0M;
+      Order testOrder = new Order(title, description, price);
+      //Act
+      string result = "wrong order";
+      //Assert
+      Assert.AreEqual(title, result);
+    }
+
   
 
   }
