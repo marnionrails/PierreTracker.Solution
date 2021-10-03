a575687bd77515c50834849a6a20c536e9460aa5
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-10-02 22:44:54 -0400

    pass test to get vendor ID

--- PierreTracker.Tests/ModelTests/VendorTests.cs
+++ PierreTracker.Tests/ModelTests/VendorTests.cs
@@ -6,8 +6,13 @@ using System;
 namespace PierreTracker.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+     public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
